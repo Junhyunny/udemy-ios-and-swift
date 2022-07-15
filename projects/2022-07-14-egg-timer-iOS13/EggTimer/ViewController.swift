43999ca058469bd16538b83d080e93ee9b42f090
Junhyunny <kang3966@naver.com>
2022-07-15 18:32:16 +0900

    Add egg alarm sound

--- projects/2022-07-14-egg-timer-iOS13/EggTimer/ViewController.swift
+++ projects/2022-07-14-egg-timer-iOS13/EggTimer/ViewController.swift
@@ -7,17 +7,53 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let eggTimes = [
-        "Soft": 5,
-        "Medium": 7,
-        "Hard": 12
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var globalTimer: Timer?
+    var player: AVAudioPlayer?
+    let eggTimes: [String:Float] = [
+        "Soft": 3,
+        "Medium": 5,
+        "Hard": 7
     ]
+    let minute:Float = 5
     
     @IBAction func clickEgg(_ sender: UIButton) {
+
+        if globalTimer != nil {
+            globalTimer?.invalidate()
+        }
+            
+        titleLabel.text = "How do you like your eggs?"
+        progressBar.progress = 0
+        
         let hardness = sender.currentTitle
-        print(eggTimes[hardness!]!)
+        let totalSeconds = eggTimes[hardness!]! * minute
+        var seconds:Float = 0
+        
+        globalTimer = Timer.scheduledTimer (
+            withTimeInterval: 1.0, repeats: true
+        ) {
+            timer in
+            seconds = seconds + 1;
+            self.progressBar.progress = seconds / totalSeconds
+            if seconds == totalSeconds {
+                timer.invalidate()
+                self.globalTimer = nil
+                self.titleLabel.text = "DONE!"
+                self.playFinishAlarm()
+            }
+        }
+    }
+    
+    func playFinishAlarm () {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player!.play()
     }
 }
