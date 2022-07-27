4642f44a55f8ef153fd52e5f4a224efdb3538efa
Junhyunny <kang3966@naver.com>
2022-07-27 23:12:16 +0900

    Add Business Card App

---
+++ projects/2022-07-27-personal-card/2022-07-27-personal-card/_022_07_27_personal_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  _022_07_27_personal_cardApp.swift
+//  2022-07-27-personal-card
+//
+//  Created by JUNHYUN KANG on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct _022_07_27_personal_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
