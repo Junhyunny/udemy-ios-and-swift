39201dca266698c9073a7b0288f3a3a1d54d1b49
Junhyunny <kang3966@naver.com>
2023-05-28 00:09:49 +0900

    Remove all files except README.md

--- projects/2022-07-05-swift-palyground/MyPlayground.playground/Contents.swift
+++
@@ -1,5 +0,0 @@
-
-// this is a comment
-
-print("Hello \(2+3) World")
-print("The result of 5 + 3 = \(5+3)")
--- projects/2022-07-16-multiple-quizzler-iOS13/Quizzler-iOS13/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  Quizzler-iOS13
-//
-//  Created by JUNHYUN KANG on 2022/07/16.
-//  Copyright © 2022 The App Brewery. All rights reserved.
-//
-
-import Foundation
